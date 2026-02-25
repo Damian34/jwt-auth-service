bc9b68ec2a7202283e28fc07a77c59750d38da46
Damian34 <dam.wadolowski@gmail.com>
2026-02-25 10:26:25 +0100

    fix

--- src/main/java/com/example/security/auth/context/refreshtoken/model/RefreshTokenRepository.java
+++ src/main/java/com/example/security/auth/context/refreshtoken/model/RefreshTokenRepository.java
@@ -1,6 +1,6 @@
 package com.example.security.auth.context.refreshtoken.model;
 
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 
